10d856a9494f626a0ba9ca05d0561c36848db65a
alejandro <alejandrojsrv@gmail.com>
2019-07-17 17:09:02 -0500

    subir

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,19 +8,29 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
-export class AppComponent {
-  constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
-  ) {
+export class AppComponent { 
+  
+  constructor( private platform: Platform, private splashScreen: SplashScreen, private statusBar: StatusBar) {
+
     this.initializeApp();
   }
+
+
 
   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+
+      var notificationOpenedCallback = function(jsonData) {
+        console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+      };
+
+      window["plugins"].OneSignal
+      .startInit("b7aa0525-7fd6-457b-9c9f-646b8d645585", "notas-62844")
+      .handleNotificationOpened(notificationOpenedCallback)
+      .endInit();
+      
     });
   }
 }
